b3316c7d34194de71563e1e667c5e3b00ef546d4
user <user@users-Mac.local>
2014-09-15 19:18:08 +0200

    setup empty master/detail project with cocoapods

---
+++ Max Client/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Max Client
+//
+//  Created by user on 15/09/14.
+//  Copyright (c) 2014 Jonas Witt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ Max Client/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Max Client
+//
+//  Created by user on 15/09/14.
+//  Copyright (c) 2014 Jonas Witt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
